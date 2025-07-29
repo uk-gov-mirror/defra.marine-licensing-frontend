fb0771aa33f34debcdcbbe3caab5f033b6d4b9af
Gareth Ferguson <gareth.ferguson@equalexperts.com>
2025-07-29 13:52:20 +0100

    Redirect to dashboard when coming from management URL

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -1,10 +1,20 @@
+import { config } from '~/src/config/config.js'
+import { routes } from '~/src/server/common/constants/routes.js'
+
 /**
  * A GDS styled example home page controller.
  * Provided as an example, remove or modify as required.
  * @satisfies {Partial<ServerRoute>}
  */
 export const homeController = {
-  handler(_request, h) {
+  handler(request, h) {
+    const { accountManagementUrl } = config.get('defraId')
+    const referer = request.headers.referer
+
+    if (referer?.indexOf(accountManagementUrl) >= 0) {
+      return h.redirect(routes.DASHBOARD)
+    }
+
     return h.redirect('/exemption')
   }
 }
--- src/server/home/controller.test.js
+++ src/server/home/controller.test.js
@@ -1,5 +1,7 @@
 import { createServer } from '~/src/server/index.js'
 import { statusCodes } from '~/src/server/common/constants/status-codes.js'
+import { config } from '~/src/config/config.js'
+import { routes } from '~/src/server/common/constants/routes.js'
 
 describe('#homeController', () => {
   /** @type {Server} */
@@ -14,10 +16,36 @@ describe('#homeController', () => {
     await server.stop({ timeout: 0 })
   })
 
-  test('Should provide expected response', async () => {
+  test('Should redirect to exemption when no referer header', async () => {
     const { headers, statusCode } = await server.inject({
       method: 'GET',
       url: '/'
+    })
+    expect(statusCode).toBe(statusCodes.redirect)
+    expect(headers.location).toBe('/exemption')
+  })
+
+  test('Should redirect to dashboard when coming from account management page', async () => {
+    const { accountManagementUrl } = config.get('defraId')
+
+    const { headers, statusCode } = await server.inject({
+      method: 'GET',
+      url: '/',
+      headers: {
+        referer: `${accountManagementUrl}`
+      }
+    })
+    expect(statusCode).toBe(statusCodes.redirect)
+    expect(headers.location).toBe(routes.DASHBOARD)
+  })
+
+  test('Should redirect to exemption when referer is not from account management page', async () => {
+    const { headers, statusCode } = await server.inject({
+      method: 'GET',
+      url: '/',
+      headers: {
+        referer: 'http://localhost:3000'
+      }
     })
     expect(statusCode).toBe(statusCodes.redirect)
     expect(headers.location).toBe('/exemption')
