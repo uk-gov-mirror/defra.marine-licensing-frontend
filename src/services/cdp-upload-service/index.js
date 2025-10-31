c85361ed53bd427e416a7af02eda0c2843c19443
Jon Shipley <jon.shipley@esynergy.co.uk>
2025-10-31 12:18:36 +0000

    Fix sonar lint

--- src/services/cdp-upload-service/index.js
+++ src/services/cdp-upload-service/index.js
@@ -1,11 +1,11 @@
-import {
-  CdpUploadService,
-  UPLOAD_STATUSES,
-  CDP_ERROR_CODES
-} from './cdp-upload-service.js'
+import { CdpUploadService } from './cdp-upload-service.js'
 
 export function getCdpUploadService(allowedMimeTypes = null) {
   return new CdpUploadService(allowedMimeTypes)
 }
 
-export { CdpUploadService, UPLOAD_STATUSES, CDP_ERROR_CODES }
+export {
+  CdpUploadService,
+  UPLOAD_STATUSES,
+  CDP_ERROR_CODES
+} from './cdp-upload-service.js'
