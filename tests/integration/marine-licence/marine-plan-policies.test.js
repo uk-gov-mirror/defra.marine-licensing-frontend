b77a6a91d5ae660ba58825f8897009bfa8b43378
Jon Shipley <jon.shipley@esynergy.co.uk>
2026-09-21 14:07:28 +0100

    style(ml-1510): format

--- tests/integration/marine-licence/marine-plan-policies.test.js
+++ tests/integration/marine-licence/marine-plan-policies.test.js
@@ -69,8 +69,9 @@ describe('Marine plan policies (policy list) page', () => {
       'href',
       marineLicenceRoutes.MARINE_LICENCE_TASK_LIST
     )
-    expect(
-      getByRole(document, 'link', { name: 'Back' })
-    ).toHaveAttribute('href', marineLicenceRoutes.MARINE_LICENCE_TASK_LIST)
+    expect(getByRole(document, 'link', { name: 'Back' })).toHaveAttribute(
+      'href',
+      marineLicenceRoutes.MARINE_LICENCE_TASK_LIST
+    )
   })
 })
